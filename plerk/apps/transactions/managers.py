8bb7a84d61219b989bed2e9b443e75c96b0d7cbe
Hugo Silva <hugofer93@gmail.com>
2022-05-19 18:09:04 -0500

    Fix and Add queries for Transactions

--- plerk/apps/transactions/managers.py
+++ plerk/apps/transactions/managers.py
@@ -1,4 +1,5 @@
-from django.db.models import Manager, Sum
+from django.db.models import Count, Manager, Sum
+from django.db.models.functions import TruncDate
 
 from plerk.apps.utils.managers import BaseQuerySet
 
@@ -52,7 +53,7 @@ class TransactionQuerySet(BaseQuerySet):
             QuerySet: Transaction list.
         """
         transactions = self.select_related('company')\
-            .self.get_successful_transactions()\
+            .get_successful_transactions()\
             .filter(company_id=company_id)
         return transactions
 
@@ -66,10 +67,24 @@ class TransactionQuerySet(BaseQuerySet):
             QuerySet: Transaction list.
         """
         transactions = self.select_related('company')\
-            .self.get_rejected_transactions()\
+            .get_rejected_transactions()\
             .filter(company_id=company_id)
         return transactions
 
+    def get_date_most_success_transactions_company(self, company_id):
+        """Get date with most successful transactions of the company.
+
+        Args:
+            company_id (UUID): Company ID.
+
+        Returns:
+            str: Only date without time.
+        """
+        date = self.get_succes_transactions_company(company_id)\
+            .values('date').annotate(trunc_date=TruncDate('date'))\
+            .values('trunc_date').annotate(count_date=Count('trunc_date'))\
+            .order_by('-count_date').first().get('trunc_date')
+        return date
 
 class TransactionManager(Manager.from_queryset(TransactionQuerySet)):
     """"Transaction Manager."""
