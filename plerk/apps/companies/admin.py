a5c2d149e8f37d7393f117016fe02c1fc5bf6e8b
Hugo Silva <hugofer93@gmail.com>
2022-05-19 01:22:03 -0500

    Add models to admin site

--- plerk/apps/companies/admin.py
+++ plerk/apps/companies/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from plerk.apps.companies.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'creation_date', 'is_active')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_filter = ('creation_date', 'is_active')
+    readonly_fields = ('id', 'name', 'creation_date', 'is_active')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- plerk/apps/transactions/admin.py
+++ plerk/apps/transactions/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from plerk.apps.transactions.models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'company', 'price', 'date', 'status', 'status_approved',
+        'final_payment', 'creation_date', 'is_active'
+    )
+    list_display_links = ('id', 'company')
+    search_fields = ('company__name', 'status')
+    list_filter = (
+        'company__name', 'date', 'status',
+        'status_approved', 'final_payment'
+    )
+    readonly_fields = (
+        'id', 'company', 'price', 'date', 'status',
+        'status_approved','final_payment', 'is_active'
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
