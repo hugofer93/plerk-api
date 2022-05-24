4cf12e58001ee6b4d1df4ab738e0434454475a4b
Hugo Silva <hugofer93@gmail.com>
2022-05-24 16:44:19 -0500

    Add settings for AdminSite

--- plerk/apps/core/admin.py
+++ plerk/apps/core/admin.py
@@ -1,4 +1,4 @@
-from django.contrib.admin import register as admin_resgiter
+from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
@@ -6,6 +6,12 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 UserModel = get_user_model()
 
 
-@admin_resgiter(UserModel)
+# Custom AdminSite
+class AdminSite(admin.AdminSite):
+    admin.AdminSite.site_header = 'PLERK Admin'
+    admin.AdminSite.site_title = admin.AdminSite.site_header
+
+
+@admin.register(UserModel)
 class UserAdmin(DjangoUserAdmin):
     """Custom User Admin for AdminSite"""
